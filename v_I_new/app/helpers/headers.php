f7ce1049bb985cadd930a3ab103e199247c94802
Anelsonmousse <138482174+Anelsonmousse@users.noreply.github.com>
2024-10-15 23:32:41 +0100

    Add files via upload

---
+++ v_I_new/app/helpers/headers.php
@@ -0,0 +1,44 @@
+<?php
+ 
+header ("Access-Control-Allow-Origin: * ") ;
+header ("Access-Control-Allow-Headers: Origin, Content-Type,Accept,Authorization");
+header ("Content-Type: application/json");
+
+include_once('jwt_helper.php');
+
+$JWT_token = 'non_authorised';
+$raw = file_get_contents('php://input');
+$data = json_decode($raw,true);
+ 
+$apiDb = '';
+    
+ if(json_encode($data) === 'null') {
+    
+	$apiDb = $_POST['requestID'];
+
+
+}else{
+
+  $apiDb = $data['requestID'];
+
+}
+
+
+
+
+if (!isset($apiDb)) {
+	$response = array(
+	  'status' => false,
+
+	  'message' => 'incomplete request !-hfile',
+
+	);
+
+	print_r(json_encode($response));
+	exit;
+  }
+
+$apiDb = 'link_54321';
+define('DB_NAME', 'smfhervm_api_'.$apiDb );
+define('ASSETS', 'assets_'.$apiDb);
+	
